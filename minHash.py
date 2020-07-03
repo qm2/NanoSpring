41648eabcce3549fe599ebfeaea899d7bb95919d
fanzhuyifan <zhuyifan@stanford.edu>
2020-07-03 08:54:33 -0700

    super slow implementation of minhash

--- minHash.py
+++ minHash.py
@@ -1,35 +1,43 @@
-import unittest, numpy as np
+import unittest
+import numpy as cp
 
 class NanoRead:
     """
     An instance of class NanoRead is one read sequence
     """
 
-    def __init__(self, line1, line2):
+    def __init__(self, index, line1, line2):
         """
         Initializes a read from two lines in the .reads file
         """
         line1Split = line1.split(':')
         self.pos = int(line1Split[0])
         self.editString = line1Split[1]
+        self.index = index
         self.data = line2
+
+    def __str__(self):
+        return "%d %d:%.10s %.10s" % (self.index, self.pos, self.editString, self.data)
 
     def getMinHash(self, k, n):
         """
-        Returns a numpy array of size n which is a MinHash sketch of the read
+        Returns a numpy array of size n which is a MinHash sketch of the read.
+        For speed reasons don't use this!
         """
+        if (self.index % 100 == 0):
+            print("Building minHash of read " + str(self))
         try:
             if self.k == k:
                 if self.n == n:
                     return self.minHash
             self.k = k
             self.n = n
-            self.minHash = NanoRead.__calcMinHash(self.k, self.n)
+            self.minHash = self.__calcMinHash(self.k, self.n)
             return self.minHash
         except AttributeError:
             self.k = k
             self.n = n
-            self.minHash = NanoRead.__calcMinHash(self.k, self.n)
+            self.minHash = self.__calcMinHash(self.k, self.n)
             return self.minHash
 
     def __calcMinHash(self, k, n):
@@ -38,11 +46,11 @@ class NanoRead:
         """
         l = len(self.data)
         numKMers = l - k + 1
-        hashes = np.zeros((numKMers, n), dtype=(np.uint32))
+        hashes = cp.zeros((numKMers, n), dtype=(cp.uint32))
         for i in range(numKMers):
             hashes[i, :] = NanoRead.calcHash(self.data[i:i + k], n)
 
-        return np.amin(hashes, axis=0)
+        return cp.amin(hashes, axis=0)
 
     @staticmethod
     def calcHash(kMer, n):
@@ -50,7 +58,7 @@ class NanoRead:
         Returns a numpy array of n different hashes of kMer. After the first
         hash is calculated, the other hashes are just bit rotations + xor
         """
-        hashes = np.zeros((n,), dtype=(np.uint32))
+        hashes = cp.zeros((n,), dtype=(cp.uint32))
         hashes[0] = hash(kMer)
         INT_BITS = 32
         # Number of bits to rotate left
@@ -70,6 +78,31 @@ class NanoReads:
         """
         Initializes the data from fileName.reads
         """
+        self.reads = []
+        i = 0
+        with open(fileName + ".reads") as f:
+            while (True):
+                l = f.readline()
+                if (l == ''):
+                    break
+                self.reads.append(NanoRead(i, l[:-1], f.readline()[:-1]))
+                i += 1
+        self.sortedReads = self.reads[:]
+        self.sortedReads.sort(key = lambda nR : nR.pos)
+        self.readLen = len(self.reads[0].data)
+        self.numReads = len(self.reads)
+
+    def buildHashTables(self, k, n):
+        """
+        Builds n Hash Tables with the sketches obtained by MinHash on k-mers
+        """
+        self.sketches = cp.array([nR.getMinHash(k, n) for nR in self.reads])
+        self.hashTables = []
+        for i in range(n):
+            self.hashTables.append({})
+            for j in range(self.numReads):
+                self.hashTables[i][self.sketches[j, i]] = self.reads[j]
+            
 
 
 class TestNanoRead(unittest.TestCase):
@@ -78,7 +111,7 @@ class TestNanoRead(unittest.TestCase):
         pos = 339
         editString = 'u4dAsTG'
         data = 'ATTTGC'
-        n = NanoRead(str(pos) + ':' + editString, data)
+        n = NanoRead(0, str(pos) + ':' + editString, data)
         self.assertEqual(n.pos, pos)
         self.assertEqual(n.editString, editString)
         self.assertEqual(n.data, data)
@@ -88,8 +121,8 @@ class TestNanoRead(unittest.TestCase):
         editString = 'u4dAsTG'
         data1 = 'ATTTGCGCCCTACCGT'
         data2 = 'ATCTGCACCCTACCCC'
-        nR1 = NanoRead(str(pos) + ':' + editString, data1)
-        nR2 = NanoRead(str(pos) + ':' + editString, data2)
+        nR1 = NanoRead(0, str(pos) + ':' + editString, data1)
+        nR2 = NanoRead(1, str(pos) + ':' + editString, data2)
         sketch1 = nR1.getMinHash(3, 4)
         sketch2 = nR2.getMinHash(3, 4)
         print(sketch1 == sketch2)
@@ -99,6 +132,14 @@ class TestNanoRead(unittest.TestCase):
         print(NanoRead.calcHash(kMer, 8))
         print(NanoRead.calcHash(kMer, 8))
 
+    def testNanoReads(self):
+        fileName = "test1"
+        nanoReads = NanoReads(fileName)
+        #print('\n'.join(map(str, nanoReads.reads)))
+        #print('\n'.join(map(str, nanoReads.sortedReads)))
+        #print(nanoReads.readLen)
+        print("Building Hash Tables");
+        nanoReads.buildHashTables(9, 8)
 
 if __name__ == '__main__':
     unittest.main()
