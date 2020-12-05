58d566e0c7a74555676f830150901b9a693f4004
Yifan Zhu <zhuyifan@stanford.edu>
2020-12-05 10:50:15 +0800

    Update validate result to support fastq files

--- validateResult.sh
+++ validateResult.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-diff -s <(sed '1~2d' "$1") "$2"
+# Usage: ./validateResult.sh originalFile decompressedFile 
+# originalFile must end in .reads or .fastq
+
+ext="${1##*.}"
+if [ "$ext" = "reads" ]; then
+    diff -s <(sed '1~2d' "$1") "$2"
+elif [ "$ext" = "fastq" ]; then
+    diff -s <(sed '1~4d;3~4d;4~4d;' "$1") "$2"
+else
+    echo "Unknown extension: $1"
+    exit
+fi
+
 if [[ $? -eq 0 ]]; then
     echo "Succeeded"
 else
