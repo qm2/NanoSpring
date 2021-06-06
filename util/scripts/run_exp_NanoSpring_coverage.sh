9e9db9fa984727e55b4d8ca8a47d844eebcdffa7
qm2 <qm2@illinois.edu>
2021-06-06 06:54:22 -0700

    some minor updates to the scripts

--- util/scripts/run_exp_NanoSpring_coverage.sh
+++ util/scripts/run_exp_NanoSpring_coverage.sh
@@ -2,5 +2,8 @@
 echo "The input fastq file path is:" $1
 echo "The log file path is:" $2
 
-stdbuf -oL /usr/bin/time -v ./../../build/NanoSpring -c -i $1 -o $1".NanoSpring" |& tee -a $2
+stdbuf -oL /usr/bin/time -v /raid/qingxi/NanoSpring-test/build/NanoSpring -c -i $1"_25x.fastq" -o $1"_25x.fastq.NanoSpring" |& tee -a $2
 
+stdbuf -oL /usr/bin/time -v /raid/qingxi/NanoSpring-test/build/NanoSpring -c -i $1"_50x.fastq" -o $1"_50x.fastq.NanoSpring" |& tee -a $2
+
+stdbuf -oL /usr/bin/time -v /raid/qingxi/NanoSpring-test/build/NanoSpring -c -i $1"_100x.fastq" -o $1"_100x.fastq.NanoSpring" |& tee -a $2
